7a8a7ed3d7c3ce0d932bedae30097fcaa7ccf2ab
tobestfol2 <tobestfol2@gmail.com>
2023-11-24 15:53:09 -0800

    configuration management

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Puppet Manifest to Create a File
+
+file { '/tmp/school':
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
---
+++ 0x0A-configuration_management/README.md
@@ -0,0 +1,4 @@
+0x0A. Configuration management
+0. Create a file
+1. Install a package
+2. Execute a command
